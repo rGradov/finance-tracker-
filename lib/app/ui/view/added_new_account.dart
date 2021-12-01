a9bccc2ab96a8a4c18dba74f401580f11f863138
vrudenia <vrudenia@asist-lab.com>
2021-12-01 22:30:32 +0300

    added ui for wallet screen

--- lib/app/ui/view/added_new_account.dart
+++ lib/app/ui/view/added_new_account.dart
@@ -23,14 +23,23 @@ class AddedNewAccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColor.violet[100]!,
-      body: SafeArea(
-        child: SizedBox.expand(
-          child: Column(
-            children: const [
-              TopNavigation(),
-            ],
+    return ChangeNotifierProvider(
+      create: (_)=>AddedNewWalletViewModel(),
+      child: Scaffold(
+        backgroundColor: AppColor.violet[100]!,
+        body: SafeArea(
+          child: SizedBox.expand(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                TopNavigation(),
+                Spacer(flex: 6),
+                _BalanceInput(),
+                SizedBox(height: 20),
+                Expanded(flex: 5, child: _InputWalletData()),
+              ],
+            ),
           ),
         ),
       ),
@@ -98,10 +107,78 @@ class TopNavigationBackButton extends StatelessWidget {
 
 /// FIXME: change my name ( make it more better )
 /// it should be rounded container with dropdown and inputted field
-///
+///IMPORTANT!! DON'T USE EXPANDED IN THIS
 class _InputWalletData extends StatelessWidget {
   const _InputWalletData({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
+
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            _InputWalletName(),
+            _DropDownAccountType(),
+            _ContinueButton(),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+class _DropDownAccountType extends StatefulWidget {
+  const _DropDownAccountType({Key? key}) : super(key: key);
+
+  @override
+  _DropDownAccountTypeState createState() => _DropDownAccountTypeState();
+}
+
+class _DropDownAccountTypeState extends State<_DropDownAccountType> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _controller,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        fillColor: Colors.green,
+        filled: false,
+        hintStyle: const TextStyle(color: Colors.blue,fontSize: 12),
+        hintText: 'name',
+      ),
+    );
+  }
+}
+
+class _InputWalletName extends StatefulWidget {
+  const _InputWalletName({Key? key}) : super(key: key);
+
+  @override
+  __InputWalletNameState createState() => __InputWalletNameState();
+}
+
+class __InputWalletNameState extends State<_InputWalletName> {
   @override
   Widget build(BuildContext context) {
     return Container();
@@ -114,7 +191,22 @@ class _BalanceInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final _textStyle = TextStyle(
+        color: AppColor.baseLight[80],
+        fontFamily: 'Inter',
+        fontWeight: FontWeight.w600,
+        fontSize: 18);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Balance', style: _textStyle),
+          Text('\$00.0', style: _textStyle.copyWith(fontSize: 64)),
+        ],
+      ),
+    );
   }
 }
 
